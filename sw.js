fba7cba2555b35c660198f08468531c6afd7794e
liamhicks2586-oss <liamhicks2586@gmail.com>
2026-03-30 03:33:44 +0100

    Update sw.js
    New update

--- sw.js
+++ sw.js
@@ -15,22 +15,14 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      ).then(() => self.clients.claim())
-    )
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    }))).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request).then(r => r || caches.match('./index.html'))
-    )
+    fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
   );
 });
