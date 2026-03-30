febc94a32d9f27c2371f18ad9d0d948abf39b760
liamhicks2586-oss <liamhicks2586@gmail.com>
2026-03-30 02:38:06 +0100

    Update sw.js
    V2 update

--- sw.js
+++ sw.js
@@ -1,30 +1,36 @@
-const CACHE_NAME = 'pub-darts-v1';
-const urlsToCache = [
+const CACHE_NAME = 'pub-darts-v3';
+const APP_SHELL = [
   './',
   './index.html',
   './display.html',
   './manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache).catch(err => {
-          console.log('Cache addAll error:', err);
-        });
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      ).then(() => self.clients.claim())
+    )
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-      .catch(() => {
-        return caches.match('./index.html');
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
